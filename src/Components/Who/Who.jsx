1adea27b0719849a313582d92113e485fe782b54
Jens Rosseel <slaythehorizon.games@gmail.com>
2022-03-30 17:47:40 +0200

    Change layout and css for who page

--- src/Components/Who/Who.jsx
+++ src/Components/Who/Who.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Who.css';
+import codehead from '../../Assets/codehead.png'
 
 const Who = () => {
   return (
-  <div id="who" className="who-container">
+  <div className="who-container">
     <div className="who-text">
-      Hello, My name is Jens Rosseel and I am a <br />
-      Web Developer With a passion for &#60;code/&#62;
+      <p>
+        Hello, My name is Jens Rosseel and I am a <br />
+        Web Developer With a passion for &#60;code/&#62;
+      </p>
+      <p className='subtext'>front-end / back-end</p>
+      <Link to='/contact'>Contact Me!</Link>
+    </div>
+    <div className='who-glitch'>
+    <div className='glitch'>
+      <img src={codehead} alt='head with code inside'/>
+      <div class="glitch__layers">
+        <div class="glitch__layer"></div>
+        <div class="glitch__layer"></div>
+        <div class="glitch__layer"></div>
+      </div>
+    </div>
     </div>
   </div>
   );
