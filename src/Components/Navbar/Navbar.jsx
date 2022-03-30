20385853946ea904cfed15e908005a8d7ffc3feb
Jens Rosseel <slaythehorizon.games@gmail.com>
2022-03-30 20:18:56 +0200

    Add responsiveness to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { FaRocket, FaAddressCard, FaMailBulk, FaCode } from "react-icons/fa";
+import { IoIosMenu } from "react-icons/io";
 
 const Navbar = () => {
+  
+  const [style, setStyle] = useState('navbar')
+
+  const handleHamburger = () => {
+    if(style == 'navbar')
+    {
+      setStyle('navbar-mobile')
+    }
+    else
+    {
+      setStyle('navbar')
+    }
+  }
+
   return (
-    <div id="navbar" className="navbar">
-        <Link to="/"><FaAddressCard/><div className='nav-text'>About</div></Link>
-        <Link to="/skills"><FaCode/><div className='nav-text'>Skills</div></Link>
-        <Link to="/projects"><FaRocket/><div className='nav-text'>Projects</div></Link>
-        <Link to="/contact"><FaMailBulk/><div className='nav-text'>Contact</div></Link>
-    </div>
+    <>
+      <div className='hamburger' onClick={handleHamburger}><IoIosMenu /></div>
+      <div className={style}>
+          <Link to="/" onClick={handleHamburger}><FaAddressCard/><div className='nav-text'>About</div></Link>
+          <Link to="/skills" onClick={handleHamburger}><FaCode/><div className='nav-text'>Skills</div></Link>
+          <Link to="/projects" onClick={handleHamburger}><FaRocket/><div className='nav-text'>Projects</div></Link>
+          <Link to="/contact" onClick={handleHamburger}><FaMailBulk/><div className='nav-text'>Contact</div></Link>
+      </div>
+    </>
   )
 };
 
