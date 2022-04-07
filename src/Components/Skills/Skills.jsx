adaa42dba2fc82dfc914ca97260b25b84c61f747
Jens Rosseel <slaythehorizon.games@gmail.com>
2022-04-07 22:45:19 +0200

    Change skills-text content in skills page

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -8,16 +8,16 @@ const Skills = () => {
         <div className='skills-text'>
             <div className='skills-title'>Skills</div>
             <p>
-                I create successful responsive websites that are fast and easy to use. 
-                The main area of my expertise is front-end development, HTML, CSS, JS, building small and medium 
-                web apps and coding interactive layouts. I also have some experience with php and the laravel framework for back-end.
+                I create responsive websites that are fast and easy to use. 
+                The main area of my expertise is HTML, CSS, JS, PHP, building small and medium 
+                web apps and coding interactive layouts. I also have experience with react and laravel frameworks.
             </p>
         </div>
         <div className='skills-visualiser'>
             <div className='skill-name'>Front-End</div>
             <LinearProgress variant='determinate' color="success" value={90} />
             <div className='skill-name'>Back-End</div>
-            <LinearProgress variant='determinate' color="error" value={80} />
+            <LinearProgress variant='determinate' color="error" value={90} />
             <div className='skill-name'>React</div>
             <LinearProgress variant='determinate' color="info" value={65} />
             <div className='skill-name'>Laravel</div>
