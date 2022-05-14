2e686ae574843825ddcf0d910e9760b4bb5e9f2f
Jens Rosseel <slaythehorizon.games@gmail.com>
2022-05-14 21:23:03 +0200

    Change projects page and edit css

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,30 +1,65 @@
-import React from 'react'
-import './Skills.css'
-import LinearProgress from '@mui/material/LinearProgress'
+import React from "react";
+import "./Skills.css";
 
-const Skills = () => { 
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiReact,
+  SiPhp,
+  SiLaravel,
+  SiMysql,
+  SiPython,
+} from "react-icons/si";
+
+const Skills = () => {
   return (
-    <div className='skills-container'>
-        <div className='skills-text'>
-            <div className='skills-title'>Skills</div>
-            <p>
-                I create responsive websites that are fast and easy to use. 
-                The main area of my expertise is HTML, CSS, JS, PHP, building small and medium 
-                web apps and coding interactive layouts. I also have experience with react and laravel frameworks.
-            </p>
-        </div>
-        <div className='skills-visualiser'>
-            <div className='skill-name'>Front-End</div>
-            <LinearProgress variant='determinate' color="success" value={90} />
-            <div className='skill-name'>Back-End</div>
-            <LinearProgress variant='determinate' color="error" value={90} />
-            <div className='skill-name'>React</div>
-            <LinearProgress variant='determinate' color="info" value={65} />
-            <div className='skill-name'>Laravel</div>
-            <LinearProgress variant='determinate' color="secondary" value={60} />
-        </div>
+    <div className="skills-container">
+      <div className="skills-text">
+        <div className="skills-title">Skills</div>
+        <p>
+          I create responsive websites that are fast and easy to use. The main
+          area of my expertise is HTML, CSS, JS, PHP, building small and medium
+          web apps and coding interactive layouts. I also have experience with
+          react and laravel frameworks.
+        </p>
+      </div>
+      <div className="skills-visualiser">
+          <ul className="orbit-wrap">
+            <li className="orbit-center">
+            </li>
+            <li>
+              <ul className="ring-2">
+                <li>
+                  <i className="orbit-icon"><SiHtml5 /></i>
+                </li>
+                <li>
+                  <i className="orbit-icon"><SiCss3 /></i>
+                </li>
+                <li>
+                  <i className="orbit-icon"><SiJavascript /></i>
+                </li>
+                <li>
+                  <i className="orbit-icon"><SiReact /></i>
+                </li>
+                <li>
+                  <i className="orbit-icon"><SiPhp /></i>
+                </li>
+                <li>
+                  <i className="orbit-icon"><SiLaravel /></i>
+                </li>
+                <li>
+                  <i className="orbit-icon"><SiMysql /></i>
+                </li>
+                <li>
+                  <i className="orbit-icon"><SiPython /></i>
+                </li>
+              </ul>
+            </li>
+          </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
